0cc2a1ae1d47e5b4c1f6894278fc7cdc25bef720
Indragith20 <indragith20@gmail.com>
2018-10-27 01:36:05 +0530

    Added Service to get the match details by date

--- src/app/dashboard/select-match/select-match.component.ts
+++ src/app/dashboard/select-match/select-match.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { DashBoardService } from 'src/app/shared/dashboard.service';
+import { MatDatepickerInputEvent } from '@angular/material';
 
 @Component({
   selector: 'app-select-match',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./select-match.component.css']
 })
 export class SelectMatchComponent implements OnInit {
-
-  constructor() { }
+  selectedDate: Date;
+  constructor(private datePipe: DatePipe, private dashboardService: DashBoardService) { }
 
   ngOnInit() {
+  }
+
+  addEvent(event: MatDatepickerInputEvent<Date>) {
+    this.selectedDate = event.value;
+    this.getMatches();
+  }
+
+  getMatches() {
+    let searchDateString = this.datePipe.transform(this.selectedDate, 'yyyy-MM-dd');
+    searchDateString = `${searchDateString}T19:30+05:30`;
+    console.log(searchDateString);
+    this.dashboardService.getMatchByDate(searchDateString)
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
 }
--- src/app/shared/dashboard.service.ts
+++ src/app/shared/dashboard.service.ts
@@ -1,6 +1,22 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class DashBoardService {
-    constructor() {}
+    constructor(private db:AngularFireDatabase, private http: HttpClient ) { }
+
+    getMatchByDate(date: string) {
+        return new Promise((resolve, reject) => {
+            const query = this.db.database.ref('matches/matches').orderByChild('start_date').equalTo(date);
+            query.on('value', (snapshot) => {
+                if(snapshot) {
+                    console.log(snapshot.val());
+                    resolve(snapshot.val());
+                } else {
+                    reject('err')
+                }
+            });
+        });
+    }
 }
